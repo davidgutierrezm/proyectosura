6ce7046529671ffedeeb48aedfd089e38982838a
505 <505@academicos.cesde.edu.co>
2022-11-18 17:52:36 -0500

    ok

--- configuracion/configuracion/urls.py
+++ configuracion/configuracion/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from web.views import Home,Medicos
+from web.views import Home,MedicosVista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', Home, name="home"),
-    path('medicos/', Medicos, name="medicos")
+    path('medicos/', MedicosVista, name="medicos")
 ]
--- configuracion/web/views.py
+++ configuracion/web/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 from web.formularios.formularioMedico import FormularioMedico
 
+from web.models import Medicos
+
 # Create your views here.
 # renderizar es PINTAR
 def Home(request):
     return render(request,'index.html')
 
-def Medicos(request):
+def MedicosVista(request):
 
     #Debo utilizar la clase formularioMedico
     #CREAMOS ASI UN OBJETO
@@ -23,7 +25,20 @@ def Medicos(request):
         if datosRecibidos.is_valid():
             #capturamos los datos
             datos=datosRecibidos.cleaned_data
-            print(datos)
+            #LLevar mis datos hacia la BD
+            medicoNuevo=Medicos(
+                nombres=datos["nombre"],
+                apellidos=datos["apellidos"],
+                cedula=datos["cedula"],
+                tarjeta=datos["tarjetaProfesional"],
+                especialidad=datos["especialidad"],
+                jornada=datos["jornada"],
+                contacto=datos["contacto"],
+                sede=datos["sede"]
+            )
+            medicoNuevo.save()
+            print("exito en la operacion")
+           
 
 
     return render(request,'registromedicos.html',diccionario)
